42727d29e0913cf4366057ec87b770012990e630
Md Tanvir Ahmed <tanvir.bd.global@gmail.com>
2023-03-18 20:41:39 +0600

    added prev and next function for change the song

--- Music-player/music-player.js
+++ Music-player/music-player.js
@@ -1,8 +1,34 @@
+const image = document.querySelector('img')
+const title = document.getElementById('title')
+const artist = document.getElementById('artist')
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev')
 const playBtn = document.getElementById('play')
-console.log(playBtn);
 const nextBtn = document.getElementById('next')
+
+// music 
+const songs = [
+    {
+        name: 'Baarishien-Anuv-Jain',
+        displayName: 'Baarishien',
+        artist: 'Anuv Jain'
+    },
+    {
+        name: 'Jo-Tu-Na-Mila-Asim-Azhar',
+        displayName: 'Jo-Tu-Na-Mila',
+        artist: 'Asim-Azhar'
+    },
+    {
+        name: 'Khair-Mangdi-Neha-Bhasin-Harrdy-Sandhu',
+        displayName: 'Khair-Mangdi',
+        artist: 'Neha-Bhasin-Harrdy-Sandhu'
+    },
+    {
+        name: 'Yeh-Aaina-Shreya-Ghoshal',
+        displayName: 'Yeh-Aaina',
+        artist: 'Shreya-Ghoshal'
+    },
+]
 
 // check is playing
 let isPlaying = false;
@@ -27,3 +53,37 @@ function pauseSong() {
 
 // play of pause event listener 
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()))
+
+// DOM update 
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `audio/${song.name}.mp3`
+    image.src = `images/${song.name}.jpg`
+}
+
+// current song 
+let songIndex = 0;
+
+// Previous song 
+function prevSong() {
+    songIndex--;
+    console.log(songIndex);
+    loadSong(songs[songIndex])
+    playSong();
+}
+
+// next song 
+function nextSong() {
+    songIndex++;
+    console.log(songIndex);
+    loadSong(songs[songIndex])
+    playSong();
+}
+
+// On load / select first song 
+loadSong(songs[3])
+
+// event listener 
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
